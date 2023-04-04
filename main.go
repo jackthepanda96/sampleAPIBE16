09bc07e976ee2594997c480f36ca57a98bec14c6
Jerry Young <jerryyoung2911@gmail.com>
2023-04-04 13:31:38 +0700

    demo server

--- main.go
+++ main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-	"ormapi/book"
-	"ormapi/config"
-	"ormapi/routes"
-	"ormapi/user"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	cfg := config.InitSQL()
-	cfg.AutoMigrate(user.User{})
-	cfg.AutoMigrate(book.Book{})
+	// cfg := config.InitSQL()
+	// cfg.AutoMigrate(user.User{})
+	// cfg.AutoMigrate(book.Book{})
 
-	mdl := user.UserModel{}
-	mdl.SetDB(cfg)
-	ctl := user.UserController{}
-	ctl.SetModel(mdl)
+	// mdl := user.UserModel{}
+	// mdl.SetDB(cfg)
+	// ctl := user.UserController{}
+	// ctl.SetModel(mdl)
 
-	bookMdl := book.BookModel{}
-	bookMdl.SetDB(cfg)
-	bookCtl := book.BookController{}
-	bookCtl.SetModel(bookMdl)
+	// bookMdl := book.BookModel{}
+	// bookMdl.SetDB(cfg)
+	// bookCtl := book.BookController{}
+	// bookCtl.SetModel(bookMdl)
 
-	// ROUTING
-	routes.Route(e, ctl, bookCtl)
+	// // ROUTING
+	// routes.Route(e, ctl, bookCtl)
+
+	e.GET("/hello", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "Hello world")
+	})
 
 	e.Start(":8000")
 }
